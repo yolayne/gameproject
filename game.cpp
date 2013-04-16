e4a2abd5dd894891b05c418bc4d5078fd038594b
user <user@CU-CS-VM.(none)>
2013-04-16 09:58:33 -0600

    menu setup

--- game.cpp
+++ game.cpp
@@ -1,4 +1,27 @@
 #include "game.hpp"
 using namespace std;
 
-"test"
+
+
+
+
+int main(){
+ 
+  cout << "\nWelcome to a really awesome text adventure game full of suprises and death!\n";
+  int menu_selection = 0;
+  string choice_str;
+  string exit_input = "2";
+  while ( menu_selection != 2 ){
+
+    do{
+      cout << "\nWould you like to play?"<<endl;
+      cout << "1 - Yes\n";
+      cout << "2 - No\n";
+      cout << "3 - WTF\n";
+      getline ( cin, choice_str );
+      menu_selection = strtol ( choice_str.c_str(), NULL, 10 );
+    }while ( menu_selection != 2 );
+  }
+
+
+}
