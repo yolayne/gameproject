dcc0aabe02e044e4bb4a0697af789ea81154d42e
user <user@CU-CS-VM.(none)>
2013-04-16 10:28:21 -0600

    another update

--- game.cpp
+++ game.cpp
@@ -1,5 +1,7 @@
 #include "game.hpp"
 using namespace std;
+#include <string>
+#include <iostream>
 
 
 
@@ -7,20 +9,29 @@ using namespace std;
 
 int main(){
  
-  cout << "\nWelcome to a really awesome text adventure game full of suprises and death!\n";
+  cout << "\nWelcome to a really awesome text adventure game full of death and destruction!\n";
   int menu_selection = 0;
   string choice_str;
   string exit_input = "2";
-  while ( menu_selection != 2 ){
+  while ( menu_selection != 3 ){
 
     do{
       cout << "\nWould you like to play?"<<endl;
       cout << "1 - Yes\n";
-      cout << "2 - No\n";
-      cout << "3 - WTF\n";
+      cout << "2 - WTF\n";
+      cout << "3 - No\n";
+      cout << "Choose wisely: ";
       getline ( cin, choice_str );
       menu_selection = strtol ( choice_str.c_str(), NULL, 10 );
-    }while ( menu_selection != 2 );
+    }while ( menu_selection < 1 || menu_selection > 3 );
+    
+    if ( menu_selection != 3 ){
+		cout << "\n GAME ON, MOTHERFUCKER!!\n";
+		return 0;
+	}
+	else{
+		cout << "\n YOU PUSSY! GAME OVER!\n";
+	}
   }
 
 
